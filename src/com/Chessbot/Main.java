f9e161e453a07e56a7b35f29ad3b47cd7d876419
Mateo <mateobotha@gmail.com>
2023-06-26 13:50:41 +0200

    Writing a quick Demo programme to utilize the new methods added in the previous commit

--- src/com/Chessbot/Main.java
+++ src/com/Chessbot/Main.java
@@ -5,8 +5,17 @@ public class Main {
     public static void main(String[] args) {
         board = new Board("Chess");
 
-        Move mouseMove = board.allowSingleMouseMoveForWhite();
-        board.makeMove(mouseMove);
-        //System.out.println(board.makeMove(new Move(0,6,0,6)));
+
+        // Demo programme that uses the 'board.allowSingleMouseMoveForWhite()' and 'board.allowSingleMouseMoveForBlack()' to allow the user to play against themselves.
+        boolean turn = true; // true = white, false = black
+        while (true) {
+            // the game will go on forever
+            Move nextMove = (turn) ? board.allowSingleMouseMoveForWhite() : board.allowSingleMouseMoveForBlack(); // Allowing a single move for the current turn which is determined by the 'turn' boolean
+            boolean moveSuccessful = board.makeMove(nextMove); // make the move and measure if it was successful
+
+            if (moveSuccessful) {
+                turn = !turn; // if the move was successful then change the turn. If it wasn't then it will loop back and the unsuccessful side has another turn.
+            }
+        }
     }
 }
